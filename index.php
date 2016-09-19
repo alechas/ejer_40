a031483670291f5d3bb812c3db497041885d12a0
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-19 19:27:29 -0300

    s

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
 <form enctype="multipart/form-data" method="post" action="nexo.php">
 <div class ="CajaInicio Animated bounceIn">
 
-<lavel> ingrese imagen </lavel>
+<lavel> ingrese imagen </lavel><br> </br>
+//<input type="text" name= "valor" id = "valor"></input> <br> </br>
+
 <br> </br>
 <input class= "MiBotonUtn" type= "submit" value="cargar" name="cargar" id="text"> </input>
 <input class= "MiBotonUtn" type= "file" value="file" name="file" id="file"> </input> 
